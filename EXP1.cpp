a283f03a274f5ffcfdad617ab8d271ef0cc28960
Shiv <sadmsd707@gmail.com>
2026-09-07 12:07:32 +0530

    Add sorting algorithms with user selection
    Implemented multiple sorting algorithms including bubble sort, selection sort, insertion sort, quick sort, and merge sort. Added user interaction for selecting the sorting algorithm.

--- EXP1.cpp
+++ EXP1.cpp
@@ -1,1 +1,205 @@
 
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, min, temp;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        min = i;
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+
+        if (min != i)
+        {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
+
+void insertionSort(int arr[], int n)
+{
+    int i, j, temp;
+
+    for (i = 1; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+    }
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    int i, j, pivot, temp;
+
+    if (low < high)
+    {
+        pivot = arr[high];
+        i = low - 1;
+
+        for (j = low; j < high; j++)
+        {
+            if (arr[j] <= pivot)
+            {
+                i++;
+
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        j = i + 1;
+
+        quickSort(arr, low, j - 1);
+        quickSort(arr, j + 1, high);
+    }
+}
+
+void merge(int arr[], int low, int mid, int high)
+{
+    int i = low, j = mid + 1, k = 0;
+    int temp[100];
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= high)
+        temp[k++] = arr[j++];
+
+    for (i = low, k = 0; i <= high; i++, k++)
+        arr[i] = temp[k];
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    int mid;
+
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int arr[100], n, i, choice;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d integers:\n", n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nUnsorted array: ");
+    printArray(arr, n);
+
+    printf("\n1. Bubble Sort");
+    printf("\n2. Selection Sort");
+    printf("\n3. Insertion Sort");
+    printf("\n4. Quick Sort");
+    printf("\n5. Merge Sort");
+
+    printf("\nEnter choice: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+        case 1:
+            bubbleSort(arr, n);
+            break;
+
+        case 2:
+            selectionSort(arr, n);
+            break;
+
+        case 3:
+            insertionSort(arr, n);
+            break;
+
+        case 4:
+            quickSort(arr, 0, n - 1);
+            break;
+
+        case 5:
+            mergeSort(arr, 0, n - 1);
+            break;
+
+        default:
+            printf("Invalid choice");
+            return 0;
+    }
+
+    printf("\nSorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
